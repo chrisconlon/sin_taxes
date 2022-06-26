0083dfb9f4e096640d243383d5e2e2a634c12217
Chris Conlon <christopherconlon@gmail.com>
2022-06-26 14:43:42 -0400

    Update 1_Prep_States_Tax.py
    harmonize fields

--- code/1_Prep_States_Tax.py
+++ code/1_Prep_States_Tax.py
@@ -9,8 +9,7 @@ fn_cigarette = raw_dir / 'state_cigarette_rates_5.xlsx'
 fn_alcohol = raw_dir / 'state_alcohol_rates.xlsx'
 
 # Outputs
-f_cig_out = data_dir/'states_cigarettes.parquet'
-f_alcohol_out = data_dir / 'states_alcohol.parquet'
+f_out = data_dir/'states_taxes.parquet'
 
 # Constants
 Region={'Connecticut':'Northeast','Maine':'Northeast','Massachusetts':'Northeast','New Hampshire':'Northeast','Rhode Island':'Northeast','Vermont':'Northeast','New Jersey':'Northeast','New York':'Northeast','Pennsylvania':'Northeast',
@@ -32,7 +31,8 @@ states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT","DE", "FL", "GA",
 # functions to process data
 def alcohol_tax(fn_in):
 	df = pd.read_excel(fn_in, '1982-2022',skiprows=[0,1,2]).query("Year > 2006")[['State name','State abbreviation','Year','Distilled spirits','Beer','Wine']]\
-			.rename(columns={'Distilled spirits':'Spirits','Year':'panel_year'})
+			.rename(columns={'Distilled spirits':'Spirits','Year':'panel_year','State abbreviation':'State'})\
+			.query('panel_year <=2020')
 
 	df.loc[df['Spirits']=='n.a.','Spirits']=np.nan
 	df.loc[df['Wine']=='n.a.','Wine']=np.nan
@@ -52,24 +52,24 @@ def alcohol_tax(fn_in):
 		df.groupby(['Region'])['Wine'].transform(np.median)
 	)
 
+	df['panel_year'] = df['panel_year'].astype(int)
 	return df
 
 def cig_tax(fn_in, Y = 2018):
-	df_cig = pd.read_excel(fn_in, str(Y), skiprows=[0,1,2])
-	df_cig = df_cig.iloc[:27,:]
-	df_cig.drop(index=df_cig.index[0], axis=0, inplace=True)
-	first_half = df_cig[['STATE','TAX RATE']]
-	second_half = df_cig[['STATE.1','TAX RATE.1']].rename(columns={'STATE.1':'STATE','TAX RATE.1':'TAX RATE'})
-	df_cig = pd.concat([first_half, second_half], ignore_index=True)
+	df_cig = pd.read_excel(fn_in, str(Y), skiprows=[0,1,2]).iloc[:27,:]
+	first_half = df_cig[['STATE','TAX RATE']].rename(columns={'STATE':'State name','TAX RATE':'Cigarettes'})
+	second_half = df_cig[['STATE.1','TAX RATE.1']].rename(columns={'STATE.1':'State name','TAX RATE.1':'Cigarettes'})
+	df_cig = pd.concat([first_half[1:], second_half[1:]], ignore_index=True)
 	df_cig = df_cig.iloc[:-1,:]
-	df_cig['STATE'] = states
-	df_cig['TAX RATE'] = df_cig['TAX RATE']*0.01 #dollars per pack
+	df_cig['State name'] = df_cig['State name'].str.strip()
+	df_cig['State'] = states
+	df_cig['Cigarettes'] = df_cig['Cigarettes'].astype(float)*0.01 #dollars per pack
 	df_cig['panel_year'] = Y
 	return df_cig
 
 df_cig = pd.concat([cig_tax(fn_cigarette, y) for y in range(2007,2020+1)], ignore_index=True)
 df_alcohol = alcohol_tax(fn_alcohol)
 
-# save the files
-df_cig.to_parquet(f_cig_out)
-df_alcohol.to_parquet(f_alcohol_out)
+out_df = pd.merge(df_alcohol, df_cig, on=['State','panel_year'], how='outer')[['State','panel_year','Spirits','Beer','Wine','Cigarettes']]
+out_df.to_parquet(f_out)
+
